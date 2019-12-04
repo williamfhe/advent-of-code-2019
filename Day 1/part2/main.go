e0bfb62ac4f05bcdc412bbc673008f7903880b6c
williamfhe <williamfhe@gmail.com>
2019-12-04 13:13:04 +0100

    feat: add day 1

---
+++ Day 1/part2/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func readFile(path string) []string {
+	file, err := os.Open(path)
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var fileLines []string
+
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+	err = file.Close()
+
+	if err != nil {
+		log.Fatalf("failed closing file: %s", err)
+	}
+
+	return fileLines
+}
+
+func main() {
+	fileLines := readFile("input.txt")
+	var total int
+
+	for _, line := range fileLines {
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatalf("Could not convert to int: %s\n", err)
+		}
+
+		for {
+			i = (i / 3) - 2
+			if i <= 0 {
+				break
+			}
+			total += i
+		}
+	}
+
+	fmt.Println(total)
+}
