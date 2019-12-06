a64f46ca62e72ea8b38db6f7f42fd89beb334b96
williamfhe <williamfhe@gmail.com>
2019-12-06 20:32:24 +0100

    feat(Day 2): clean the code

--- Day 2/part1/main.go
+++ Day 2/part1/main.go
@@ -2,49 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"strconv"
 	"strings"
 )
 
-const input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,6,23,2,13,23,27,1,27,13,31,1,9,31,35,1,35,9,39,1,39,5,43,2,6,43,47,1,47,6,51,2,51,9,55,2,55,13,59,1,59,6,63,1,10,63,67,2,67,9,71,2,6,71,75,1,75,5,79,2,79,10,83,1,5,83,87,2,9,87,91,1,5,91,95,2,13,95,99,1,99,10,103,1,103,2,107,1,107,6,0,99,2,14,0,0"
+func readInput(path string) []int {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Could not read file :%s\n", err)
+	}
 
-var opcodes []int
+	var input []int
 
-func init() {
-	splittedInput := strings.Split(input, ",")
+	splittedData := strings.Split(string(data), ",")
 
-	for _, code := range splittedInput {
-		opcode, err := strconv.Atoi(code)
+	for _, valueStr := range splittedData {
+		valueInt, err := strconv.Atoi(valueStr)
 		if err != nil {
 			log.Fatalf("Could not convert to int: %s\n", err)
 		}
-		opcodes = append(opcodes, opcode)
+		input = append(input, valueInt)
 	}
 
-	opcodes[1] = 12
-	opcodes[2] = 2
+	return input
 }
 
 func main() {
-	index := 0
+	input := readInput("input.txt")
 
-	for opcodes[index] != 99 {
-		index1 := opcodes[index+1]
-		index2 := opcodes[index+2]
-		index3 := opcodes[index+3]
+	input[1] = 12
+	input[2] = 2
 
-		switch opcodes[index] {
+	instructionIndex := 0
+	for input[instructionIndex] != 99 {
+		indexv1 := input[instructionIndex+1]
+		indexv2 := input[instructionIndex+2]
+		indexv3 := input[instructionIndex+3]
+
+		switch input[instructionIndex] {
 		case 1:
-			opcodes[index3] = opcodes[index1] + opcodes[index2]
+			input[indexv3] = input[indexv1] + input[indexv2]
 		case 2:
-			opcodes[index3] = opcodes[index1] * opcodes[index2]
+			input[indexv3] = input[indexv1] * input[indexv2]
 		default:
-			log.Fatalf("Unknown opcode %d at %d\n", opcodes[index], index)
+			log.Fatalf("Unknown opcode %d at %d\n", input[instructionIndex], instructionIndex)
 		}
 
-		index += 4
+		instructionIndex += 4
 	}
 
-	fmt.Println(opcodes[0])
+	fmt.Println(input[0])
 }
--- Day 2/part2/main.go
+++ Day 2/part2/main.go
@@ -2,65 +2,74 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"strconv"
 	"strings"
 )
 
-const input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,6,23,2,13,23,27,1,27,13,31,1,9,31,35,1,35,9,39,1,39,5,43,2,6,43,47,1,47,6,51,2,51,9,55,2,55,13,59,1,59,6,63,1,10,63,67,2,67,9,71,2,6,71,75,1,75,5,79,2,79,10,83,1,5,83,87,2,9,87,91,1,5,91,95,2,13,95,99,1,99,10,103,1,103,2,107,1,107,6,0,99,2,14,0,0"
-
 const expectedOutput = 19690720
 
-var opcodes []int
+func readInput(path string) []int {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Could not read file :%s\n", err)
+	}
 
-func init() {
-	splittedInput := strings.Split(input, ",")
+	var input []int
 
-	for _, code := range splittedInput {
-		opcode, err := strconv.Atoi(code)
+	splittedData := strings.Split(string(data), ",")
+
+	for _, valueStr := range splittedData {
+		valueInt, err := strconv.Atoi(valueStr)
 		if err != nil {
 			log.Fatalf("Could not convert to int: %s\n", err)
 		}
-		opcodes = append(opcodes, opcode)
+		input = append(input, valueInt)
 	}
+
+	return input
 }
 
-func tryNounVerb(noun, verb int) int {
+func tryNounVerb(input []int, noun, verb int) int {
 
-	codes := make([]int, len(opcodes))
-	copy(codes, opcodes)
+	inputCopy := make([]int, len(input))
+	copy(inputCopy, input)
 
-	codes[1] = noun
-	codes[2] = verb
+	inputCopy[1] = noun
+	inputCopy[2] = verb
 
-	index := 0
-	for codes[index] != 99 {
-		index1 := codes[index+1]
-		index2 := codes[index+2]
-		index3 := codes[index+3]
+	instructionIndex := 0
+	for inputCopy[instructionIndex] != 99 {
+		indexv1 := inputCopy[instructionIndex+1]
+		indexv2 := inputCopy[instructionIndex+2]
+		indexv3 := inputCopy[instructionIndex+3]
 
-		switch codes[index] {
+		switch inputCopy[instructionIndex] {
 		case 1:
-			codes[index3] = codes[index1] + codes[index2]
+			inputCopy[indexv3] = inputCopy[indexv1] + inputCopy[indexv2]
 		case 2:
-			codes[index3] = codes[index1] * codes[index2]
+			inputCopy[indexv3] = inputCopy[indexv1] * inputCopy[indexv2]
 		default:
-			log.Fatalf("Unknown opcode %d at %d\n", codes[index], index)
+			log.Fatalf("Unknown opcode %d at %d\n", inputCopy[instructionIndex], instructionIndex)
 		}
 
-		index += 4
+		instructionIndex += 4
 	}
 
-	return codes[0]
+	return inputCopy[0]
 }
 
 func main() {
+	input := readInput("input.txt")
 
+loop:
 	for noun := 0; noun < 99; noun++ {
 		for verb := 0; verb < 99; verb++ {
-			output := tryNounVerb(noun, verb)
+			output := tryNounVerb(input, noun, verb)
 			if output == expectedOutput {
 				fmt.Println(noun*100 + verb)
+				break loop
 			}
 		}
 	}
